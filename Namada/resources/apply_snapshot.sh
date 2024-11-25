164d7c6382336013aa22d603cdbaa4758a0930ae
Grand Valley <cobajohnson90@gmail.com>
2024-11-25 21:07:51 +0700

    done

--- Namada/resources/apply_snapshot.sh
+++ Namada/resources/apply_snapshot.sh
@@ -7,18 +7,11 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# Snapshot URLs
-MAND_DB_SNAPSHOT_URL="https://snapshots2.mandragora.io/namada/db.lz4"
-MAND_DATA_SNAPSHOT_URL="https://snapshots2.mandragora.io/namada/data.lz4"
-MAND_API_URL="https://snapshots2.mandragora.io/namada/info.json"
-
-ITR_API_URL="https://server-5.itrocket.net/mainnet/namada/.current_state.json"
-
 # Function to display the menu
 show_menu() {
     echo -e "${GREEN}Choose a snapshot provider:${NC}"
-    echo "1. Mandragora"
-    echo "2. ITRocket"
+    echo "1. Provider 1"
+    echo "2. Provider 2"
     echo "3. Exit"
 }
 
@@ -39,7 +32,7 @@ display_snapshot_details() {
     local snapshot_info=$(curl -s $api_url)
     local snapshot_height
 
-    if [[ $api_url == *"mandragora"* ]]; then
+    if [[ $api_url == *"provider1"* ]]; then
         snapshot_height=$(echo "$snapshot_info" | grep -oP '"snapshot_height":\s*\K\d+')
     else
         snapshot_height=$(echo "$snapshot_info" | jq -r '.snapshot_height')
@@ -48,7 +41,7 @@ display_snapshot_details() {
     echo -e "${GREEN}Snapshot Height:${NC} $snapshot_height"
 
     # Get the real-time block height
-    realtime_block_height=$(curl -s -X POST "https://lightnode-json-rpc-mainnet-namada.grandvalleys.com" -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r '.result' | xargs printf "%d\n")
+    realtime_block_height=$(curl -s -X POST "https://rpc.namada-dryrun.tududes.com" -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r '.result' | xargs printf "%d\n")
 
     # Calculate the difference
     block_difference=$((realtime_block_height - snapshot_height))
@@ -57,44 +50,44 @@ display_snapshot_details() {
     echo -e "${GREEN}Block Difference:${NC} $block_difference"
 }
 
-# Function to choose snapshot type for Mandragora
-choose_mandragora_snapshot() {
-    echo -e "${GREEN}Checking availability of Mandragora snapshots:${NC}"
+# Function to choose snapshot type for Provider 1
+choose_provider1_snapshot() {
+    echo -e "${GREEN}Checking availability of Provider 1 snapshots:${NC}"
     echo -n "DB Snapshot: "
-    check_url $MAND_DB_SNAPSHOT_URL
+    check_url "https://provider1.example.com/db.lz4"
     echo -n "Data Snapshot: "
-    check_url $MAND_DATA_SNAPSHOT_URL
+    check_url "https://provider1.example.com/data.lz4"
 
     prompt_back_or_continue
 
-    display_snapshot_details $MAND_API_URL
+    display_snapshot_details "https://provider1.example.com/info.json"
 
     DB_SNAPSHOT_FILE="db.lz4"
     DATA_SNAPSHOT_FILE="data.lz4"
 }
 
-# Function to choose snapshot type for ITRocket
-choose_itrocket_snapshot() {
-    echo -e "${GREEN}Checking availability of ITRocket snapshot:${NC}"
+# Function to choose snapshot type for Provider 2
+choose_provider2_snapshot() {
+    echo -e "${GREEN}Checking availability of Provider 2 snapshot:${NC}"
     echo -n "Snapshot: "
-    check_url $ITR_API_URL
+    check_url "https://provider2.example.com/.current_state.json"
 
-    display_snapshot_details $ITR_API_URL
+    display_snapshot_details "https://provider2.example.com/.current_state.json"
 
     prompt_back_or_continue
 
-    FILE_NAME=$(curl -s $ITR_API_URL | jq -r '.snapshot_name')
-    SNAPSHOT_URL="https://server-5.itrocket.net/mainnet/namada/$FILE_NAME"
+    FILE_NAME=$(curl -s "https://provider2.example.com/.current_state.json" | jq -r '.snapshot_name')
+    SNAPSHOT_URL="https://provider2.example.com/$FILE_NAME"
 }
 
-# Function to decompress Mandragora snapshots
-decompress_mandragora_snapshots() {
+# Function to decompress Provider 1 snapshots
+decompress_provider1_snapshots() {
     lz4 -c -d $DB_SNAPSHOT_FILE | tar -xv -C $HOME/.local/share/namada/namada-dryrun.abaaeaf7b78cb3ac
     lz4 -c -d $DATA_SNAPSHOT_FILE | tar -xv -C $HOME/.local/share/namada/namada-dryrun.abaaeaf7b78cb3ac/cometbft
 }
 
-# Function to decompress ITRocket snapshot
-decompress_itrocket_snapshot() {
+# Function to decompress Provider 2 snapshot
+decompress_provider2_snapshot() {
     lz4 -c -d $SNAPSHOT_FILE | tar -xv -C $HOME/.local/share/namada/namada-dryrun.abaaeaf7b78cb3ac
 }
 
@@ -115,16 +108,16 @@ main_script() {
 
     case $provider_choice in
         1)
-            provider_name="Mandragora"
+            provider_name="Provider 1"
             echo -e "Grand Valley extends its gratitude to ${YELLOW}$provider_name${NC} for providing snapshot support."
 
-            choose_mandragora_snapshot
+            choose_provider1_snapshot
             ;;
         2)
-            provider_name="ITRocket"
+            provider_name="Provider 2"
             echo -e "Grand Valley extends its gratitude to ${YELLOW}$provider_name${NC} for providing snapshot support."
 
-            choose_itrocket_snapshot
+            choose_provider2_snapshot
             ;;
         3)
             echo -e "${GREEN}Exiting.${NC}"
@@ -161,13 +154,13 @@ main_script() {
 
     # Download and decompress snapshots based on the provider
     if [[ $provider_choice -eq 1 ]]; then
-        wget -O $DB_SNAPSHOT_FILE $MAND_DB_SNAPSHOT_URL
-        wget -O $DATA_SNAPSHOT_FILE $MAND_DATA_SNAPSHOT_URL
-        decompress_mandragora_snapshots
+        wget -O $DB_SNAPSHOT_FILE "https://provider1.example.com/db.lz4"
+        wget -O $DATA_SNAPSHOT_FILE "https://provider1.example.com/data.lz4"
+        decompress_provider1_snapshots
     elif [[ $provider_choice -eq 2 ]]; then
         SNAPSHOT_FILE=$FILE_NAME
         wget -O $SNAPSHOT_FILE $SNAPSHOT_URL
-        decompress_itrocket_snapshot
+        decompress_provider2_snapshot
     fi
 
     # Change ownership of the .local/share/namada directory
