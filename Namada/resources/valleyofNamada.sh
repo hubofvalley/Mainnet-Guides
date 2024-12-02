84b96916b4972c47d11ad24a796502bca6265aa4
Grand Valley <cobajohnson90@gmail.com>
2024-12-02 10:38:05 +0700

    done

--- Namada/resources/valleyofNamada.sh
+++ Namada/resources/valleyofNamada.sh
@@ -1061,6 +1061,9 @@ function menu() {
     echo -e "${YELLOW}6. Show Guidelines${RESET}"
     echo -e "${RED}7. Exit${RESET}"
 
+    echo -e "${YELLOW}Please run the following command to apply the changes after exiting the script:${RESET}"
+    echo -e "${GREEN}source ~/.bash_profile${RESET}"
+    echo -e "${YELLOW}This ensures the environment variables are set in your current bash session.${RESET}"
     echo -e "${GREEN}Let's Buidl Namada Together - Grand Valley${RESET}"
     read -p "Choose an option (e.g., 1a or 1 then a): " OPTION
 
