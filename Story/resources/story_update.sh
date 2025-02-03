c36e448eb0a9df687cd600b0fa4da7cca8c421cd
Grand Valley <cobajohnson90@gmail.com>
2025-02-04 03:42:40 +0700

    go version update on VoS

--- Story/resources/story_update.sh
+++ Story/resources/story_update.sh
@@ -211,7 +211,7 @@ read -p "Enter the letter corresponding to the version: " choice
 
 case $choice in
     #a)
-        #update_version "v0.12.1" "https://github.com/piplabs/story/releases/download/v0.12.1" 322000
+        #update_version "v0.12.1" "https://github.com/piplabs/story/releases/download/v0.12.1" 677886
         #;;
     #b)
         #update_version "v0.13.0" "https://github.com/piplabs/story/releases/download/v0.13.0" 858000
