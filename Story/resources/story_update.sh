cbd2569daeb365b9df773a4fc14c18c7693d63fc
Grand Valley <cobajohnson90@gmail.com>
2025-02-05 22:41:34 +0700

    done

--- Story/resources/story_update.sh
+++ Story/resources/story_update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
 # Function to install cosmovisor
 install_cosmovisor() {
     echo "Installing cosmovisor..."
