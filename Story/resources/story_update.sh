a5047a25a735f8917d411695f6ce849f962fe56b
Grand Valley <letsbuidltogether@grandvalleys.com>
2025-06-14 15:57:53 +0700

    add story v1.2.1 version upgrade into VoS

--- Story/resources/story_update.sh
+++ Story/resources/story_update.sh
@@ -196,8 +196,7 @@ echo "Choose the version to update to:"
 echo -e "a. ${YELLOW}v1.1.0${RESET} (${GREEN}Virgil${RESET} Upgrade height: 640,000)"
 echo -e "b. ${YELLOW}v1.1.1${RESET} (${GREEN}Additional update for validator CLI interaction${RESET} Upgrade height: 1,398,904)"
 echo -e "c. ${YELLOW}v1.2.0${RESET} (${GREEN}Ovid${RESET} Upgrade height: 4,000,000)"
-#echo "c. v1.3.0 (Upgrade height: 2,065,886)"
-#echo "d. Batch update: Upgrade to v1.1.0 at height 640,000, v1.1.1 at height 858,000, and v1.1.0 at height 2,065,886 (RECOMMENDED FOR THOSE AIMING TO ACHIEVE ARCHIVE NODE STATUS)."
+echo -e "d. ${YELLOW}v1.2.1${RESET} (${GREEN}Validator operations CLI improvements${RESET} Upgrade height: 5,233,000)"
 read -p "Enter the letter corresponding to the version: " choice
 
 case $choice in
@@ -210,9 +209,9 @@ case $choice in
     c)
         update_version "v1.2.0" "https://github.com/piplabs/story/releases/download/v1.2.0" 4000000
         ;;
-    #d)
-        #batch_update_version
-        #;;
+    d)
+        update_version "v1.2.1" "https://github.com/piplabs/story/releases/download/v1.2.1" 5233000
+        ;;
     *)
         echo "Invalid choice. Exiting."
         exit 1
