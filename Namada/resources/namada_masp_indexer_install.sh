8ad60768e62a8815256a88ed173061b38ecc7f2b
Grand Valley <letsbuidltogether@grandvalleys.com>
2025-10-04 12:12:48 +0700

    bump MASP Indexer version to v1.4.4

--- Namada/resources/namada_masp_indexer_install.sh
+++ Namada/resources/namada_masp_indexer_install.sh
@@ -131,7 +131,7 @@ deploy() {
     git clone https://github.com/anoma/namada-masp-indexer.git "${INDEXER_DIR}"
     cd "${INDEXER_DIR}"
     git fetch --all
-    LATEST_TAG="v1.3.0"
+    LATEST_TAG="v1.4.4"
     git checkout $LATEST_TAG
     git reset --hard $LATEST_TAG
 
